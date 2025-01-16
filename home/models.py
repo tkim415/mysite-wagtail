8e3157a95547abefbe7359631150996d70bc6007
Thomas Kim <thomasdkim@gmail.com>
2025-01-16 12:34:22 -0800

    updated models.py for HomePage

--- home/models.py
+++ home/models.py
@@ -1,7 +1,21 @@
 from django.db import models
-
-from wagtail.models import Page
-
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 class HomePage(Page):
-    pass
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255, blank=True)
+    background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('title'),
+        FieldPanel('subtitle'),
+        ImageChooserPanel('background_image'),
+    ]
